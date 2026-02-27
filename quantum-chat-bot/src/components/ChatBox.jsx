3ccafad7e97e022d05f1b735a54b574e62ecfcda
Charan-718 <charan06082004@gmail.com>
2026-02-27 15:51:19 +0530

    Squid Animated

--- quantum-chat-bot/src/components/ChatBox.jsx
+++ quantum-chat-bot/src/components/ChatBox.jsx
@@ -1,38 +1,3 @@
-// import Message from "./Message";
-// import './ChatBox.css'
-
-// function ChatBox({ messages, loading, onLanguageSelect }) {
-//   return (
-//     <div className="chat-box">
-//       {messages.map((msg, index) => (
-//         <Message
-//           key={index}
-//           sender={msg.sender}
-//           text={msg.text}
-//           onLanguageSelect={onLanguageSelect}
-//         />
-//       ))}
-
-//       {loading && (
-//         <div className="message bot typing">
-//           <strong>Assistant:</strong>
-//           <p>
-//             <span className="typing-dot" />
-//             <span className="typing-dot" />
-//             <span className="typing-dot" />
-//           </p>
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default ChatBox;
-
-
-
-
-
 import Message from "./Message";
 import './ChatBox.css'
 
